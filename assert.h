347f650eb6bd7371612f3bc0f05bb7fcdce08365
TSnake41 <TSnake@Outlook.fr>
2024-03-11 16:05:24 +0100

    Hello world with colors

--- assert.h
+++ assert.h
@@ -6,13 +6,3 @@
 int __assert(_Bool condition, const char *cond, const char *func, int line);
 
 #endif
-
-int __assert(_Bool condition, const char *cond, const char *func, int line)
-{
-  if (!condition) {
-    printf("Assertion failed (%s) %s:%d", cond, func, line);
-    exit(1);
-  }
-
-  return condition;
-}
--- helloworld.c
+++ helloworld.c
@@ -1,45 +1,25 @@
 #include <uefi.h>
 
 #include "assert.h"
-
-uint32_t *framebuffer;
+#include "framebuffer.h"
 
 int main(int argc, char **argv)
 {
-  efi_guid_t gopGuid = EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID;
-  efi_gop_t *gop;
-
-  if (EFI_ERROR(BS->LocateProtocol(&gopGuid, NULL, (void **)&gop))) {
-    printf("Unable to locate protocol\n");
-    return 1;
-  }
-
-  efi_gop_mode_info_t *info;
-  uintn_t info_size;
-
-  for (size_t i = 0; i < gop->Mode->MaxMode; i++) {
-    if (EFI_ERROR(gop->QueryMode(gop, i, &info_size, (efi_gop_mode_info_t **)&info))) {
-      printf("Unable to get modes\n");
-      return 1;
-    }
-
-    printf("%u: w:%u h:%u pf:%d\n", i,
-      info->HorizontalResolution, info->VerticalResolution,
-      info->PixelFormat);
-  }
-
-  if (EFI_ERROR(gop->QueryMode(gop, gop->Mode->Mode, &info_size, (efi_gop_mode_info_t **)&info))) {
-    printf("Unable to get current mode\n");
-    return 1;
-  }
-
-  if (info->PixelFormat != PixelRedGreenBlueReserved8BitPerColor) {
-    printf("Unsupported pixel format");
-  }
-
-  framebuffer = calloc(sizeof(uint32_t), info->HorizontalResolution * info->VerticalResolution);
-  assert(framebuffer);
-
   printf("Hello World!\n");
+
+  struct framebuffer fb;
+  
+  if (!fb_init(&fb)) {
+    printf("Framebuffer unusable");
+    return 0;
+  }
+
+  for (int x = 0; (x < 255 * 255) && (x < fb.width); x++)
+    for (int y = 0; y < fb.height; y++)
+      fb_setcolor(&fb, x, y, x % 255, x / 255, y % 255);
+
+  fb_redraw(&fb);
+
+  getchar();
   return 0;
 }
